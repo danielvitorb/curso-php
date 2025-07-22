469260d5e596a01e16daecf4c4257ef5f6c433ba
Daniel Vítor <danielvitoroliveirab@gmail.com>
2025-07-22 11:39:01 -0300

    Create tipos.php

---
+++ aula04/tipos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>  
+</head>
+<body>
+    <?php
+        $n = 4;
+        $no = "Daniel";
+        echo $no;
+    ?>
+</body>
+</html>
