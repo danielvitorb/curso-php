42b2d7ccff2979295db2b4218b194ca9f8d168f1
Daniel Vítor <danielvitoroliveirab@gmail.com>
2025-07-22 15:11:10 -0300

    aula 07

---
+++ aula06/04-variaveisvariaveis.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title>Curso de PHP - CursoemVideo.com</title>  
+</head>
+<body>
+<div>
+    <?php
+        $x = "america";
+        $$x = "def";
+        echo "O conteudo da variavel X é $x";
+        echo "<br>A variável AMERICA criada recebeu o valor $america";
+    ?>
+</div>
+</body>
+</html>
