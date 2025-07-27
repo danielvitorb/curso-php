c1e2102df5beb112c395a83e35a26336676c1e99
Daniel Vítor <danielvitoroliveirab@gmail.com>
2025-07-27 10:08:31 -0300

    aula 12

--- aula12/_modelophp.php
+++ aula12/_modelophp.php
@@ -8,7 +8,14 @@
 <body>
 <div>
     <?php
-        
+        $numero = $_GET["n"];
+        echo "Mostrando a tabuada de $numero<br>";
+        $c = 1;
+        do {
+          $r = $numero * $c;
+          echo "$numero x $c = $r<br>";
+          $c++;
+        }while($c < 11);
     ?>
 </div>
 </body>
